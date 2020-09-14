9289d09f2e13fcd9581cbecd9e329e1503bc8192
Muhammad Kuswari <muh.kuswari10@gmail.com>
2020-09-15 06:44:38 +0800

    add styling for login page

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->user()) {
+        return view("home");
+    } else {
+        return view("auth.login");
+    }
 });
 
 Auth::routes();
+
+Route::match(['get', 'post'], '/register', function () {
+    return redirect("/login");
+})->name("register");
 
 Route::get('/home', 'HomeController@index')->name('home');
