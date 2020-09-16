87f47b3e20032e260fa7cff4087b9f849f58ead6
Muhammad Kuswari <muh.kuswari10@gmail.com>
2020-09-16 17:04:04 +0800

    add active state on siderbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,30 +41,38 @@
       <hr class="sidebar-divider my-0">
 
       <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
+      <li class="nav-item {{ Request::path() == 'home' ? 'active' : '' }}">
         <a class="nav-link" href="{{ route("home") }}">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ Request::path() == 'users' ? 'active' : '' }}">
         <a class="nav-link" href="{{ route("users.index") }}">
           <i class="fas fa-fw fa-users"></i>
           <span>Manage Users</span></a>
       </li>
 
-      <li class="nav-item">
+      <li class="nav-item {{ Request::path() == 'categories' ? 'active' : '' }}">
           <a class="nav-link" href="{{ route("categories.index") }}">
             <i class="fas fa-fw fa-tags"></i>
             <span>Manage Categories</span>
             </a>
       </li>
 
-            <li class="nav-item">
+            <li class="nav-item {{ Request::path() == 'books' ? 'active' : '' }}">
           <a class="nav-link" href="{{ route("books.index") }}">
             <i class="fas fa-fw fa-book"></i>
             <span>Manage Books</span>
             </a>
       </li>
+
+                  <li class="nav-item {{ Request::path() == 'orders' ? 'active' : '' }}">
+          <a class="nav-link" href="{{ route("orders.index") }}">
+            <i class="fas fa-fw fa-cart-plus"></i>
+            <span>Manage Orders</span>
+            </a>
+      </li>
+
 
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
